0145acb75b401a3b42a770aedb5fc0df043702e1
Lei Fu <leifu@dyn3143-101.wlan.ic.ac.uk>
2019-03-17 20:37:02 +0000

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longtitude, callback) => {
             callback('Unable to find location')
         } else {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a '
-            + body.currently.precipProbability + '% chance of rain')
+            + body.currently.precipProbability + '% chance of rain' + '. The high today is ' + body.daily.data[0].temperatureHigh 
+            + ' with a low of ' + body.daily.data[0].temperatureLow + '.')
         }
     })
 }
